0f312c590d0d81c36c3a31a15ac419198c391f0a
SadafKhan434 <sadafkhan2192@gmail.com>
2026-02-24 20:45:19 +0530

    Now Updated

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -7,6 +7,7 @@ return(
         <header className={classes.header}>
             <h1>React Meals</h1>
             <HeaderCartButton/>
+            <h2>Delicious Food</h2>
             </header>
         <div className={classes['main-image']}>
         <img src="https://images.pexels.com/photos/1092730/pexels-photo-1092730.jpeg"/>
