cd5b973977cf18eca5c13d93179ad26c6cdc5392
SadafKhan434 <sadafkhan2192@gmail.com>
2026-02-24 19:45:57 +0530

    First commit

---
+++ src/Components/Cart/CartIcon.js
@@ -0,0 +1,12 @@
+const CartIcon=()=>{
+    return(
+        <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        >
+            <path d="M3 1a1 1 0 000 2h.1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43"></path>
+        </svg>
+    )
+}
+export default CartIcon;
---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+import HeaderCartButton from "./HeaderCartButton";
+import classes from"./Header.module.css";
+const Header =(props)=>{
+return(
+    <Fragment>
+        <header className={classes.header}>
+            <h1>React Meals</h1>
+            <HeaderCartButton/>
+            </header>
+        <div className={classes['main-image']}>
+        <img src="https://images.pexels.com/photos/1092730/pexels-photo-1092730.jpeg"/>
+</div>
+    </Fragment>
+)
+}
+export default Header;
