485babb59c2d371b8628aa7850c3d65b5cd49438
SadafKhan434 <sadafkhan2192@gmail.com>
2026-02-27 19:41:26 +0530

    MealsItemMessage

---
+++ src/Components/Meals/AvailableMeals.js
@@ -0,0 +1,46 @@
+import Card from '../UI/Card';
+import MealsItem from './MealsItem/MealsItem';
+import classes from './AvailableMeals.Module.css';
+const DUMMY_Meals=[
+    {
+        id:'m1',
+        name:'Sushi',
+        description:'Finest fish and veggies',
+        price:22.99,
+    },
+    {
+    id:'m2',
+    name:'Schnitzel',
+    description:'A german specialty!',
+    price:16.5,
+    },
+    {
+        id:'m3',
+        name:'Barbecue Burger',
+        description:'American,raw,meaty',
+        price:12.99,
+    },{
+        id:'m4',
+        name:'Green Bowl',
+        description:'Healthy and green',
+        price:14.98,
+    },
+];
+const AvailableMeals=()=>{
+    const mealsList = DUMMY_Meals.map((meals)=>(
+    <MealsItem 
+    key={meals.id}
+     name={meals.name}
+      description={meals.description}
+       price={meals.price}
+       />
+))
+    return(
+        <section className={classes.meals}>
+            <Card>
+            <ul>{mealsList}</ul>
+            </Card>
+        </section>
+    )
+}
+export default AvailableMeals;
