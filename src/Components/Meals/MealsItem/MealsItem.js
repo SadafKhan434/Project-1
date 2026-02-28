21681dbd66c7b775228a576c8ee184821b2a954d
SadafKhan434 <sadafkhan2192@gmail.com>
2026-02-28 12:15:04 +0530

    ButtonChange

--- src/Components/Meals/MealsItem/MealsItem.js
+++ src/Components/Meals/MealsItem/MealsItem.js
@@ -1,3 +1,4 @@
+import MealsItemForm from './MealsItemForm';
 import classes from './MealsItem.Module.css';
 const MealsItem = props =>{
     const price=`$${props.price.toFixed(2)}`;
@@ -7,7 +8,9 @@ return<li className={classes.meal}>
 <div className={classes.description}>{props.description}</div>
 <div className={classes.price}>{price}</div>
 </div>
-<div></div>
+<div>
+    <MealsItemForm/>
+</div>
 </li>
 }
 export default MealsItem;
